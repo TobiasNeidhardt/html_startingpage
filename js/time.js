ec1a1d52eff4077ec2ee60e1231f1bdbdc3531f4
Neidhardt, Tobias <tobias.neidhardt@student.fhws.de>
2021-05-21 09:55:22 +0200

    Add new images

--- js/time.js
+++ js/time.js
@@ -4,11 +4,14 @@ function get_greeting() {
 
   if (hours >= 6 && hours <=11){
     document.getElementById("greeting").innerHTML = "Guten Morgen Tobias!";
+    document.getElementById("icon").src ="images/sunrice.png"
   }
   else if (hours >11 && hours <18) {
     document.getElementById("greeting").innerHTML = "Guten Mittag Tobias!";
+    document.getElementById("icon").src ="images/sun.png"
   }
   else{
     document.getElementById("greeting").innerHTML = "Schönen Abend Tobias!";
+    document.getElementById("icon").src ="images/moon.png"
   }
 }
